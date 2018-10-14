e3472a2acac22202c8be85b615eff23943bdf04f
Tomohiro Murota <tomohiro.murota@gmail.com>
2018-10-14 14:16:06 +0900

    Add docs pages base

---
+++ docs/src/Layout.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+import Container from 'react-bulma-components/lib/components/container';
+import Content from 'react-bulma-components/lib/components/content';
+import Footer from 'react-bulma-components/lib/components/footer';
+import Heading from 'react-bulma-components/lib/components/heading';
+import Section from 'react-bulma-components/lib/components/section';
+import Tile from 'react-bulma-components/lib/components/tile';
+
+const Layout = () => (
+  <div>
+	  <Section>
+
+	    <Container>
+	      <Heading>Privacy Policy</Heading>
+
+	      <Tile notification color="primary">
+
+	      </Tile>
+	    </Container>
+
+	  </Section>
+
+	  <Footer>
+      <Container>
+	      <Content style={{ textAlign: 'center' }}>
+	        <p>
+	          <strong>Breakthrough</strong> by <a href="https://tearoom6.github.io/">tearoom6</a>. All rights reserved.
+	        </p>
+	      </Content>
+	    </Container>
+	  </Footer>
+  </div>
+)
+
+export default Layout
---
+++ docs/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Layout from './Layout'
+
+ReactDOM.render(
+  <Layout/>,
+  document.getElementById('root')
+)
