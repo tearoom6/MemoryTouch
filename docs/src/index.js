c7c1b2feb6e56220f1f1bda41cc4990cd0b0bdb5
Tomohiro Murota <tomohiro.murota@gmail.com>
2019-01-14 14:14:05 +0900

    Add webfontloader

--- docs/src/index.js
+++ docs/src/index.js
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom'
 import i18next from 'i18next'
 import Backend from 'i18next-xhr-backend'
 import LngDetector from 'i18next-browser-languagedetector'
+import WebFont from 'webfontloader'
 
 import Privacy from './Privacy'
+
+WebFont.load({
+  google: {
+    families: ['M PLUS 1p', 'Montserrat']
+  }
+})
 
 i18next
   .use(LngDetector)
